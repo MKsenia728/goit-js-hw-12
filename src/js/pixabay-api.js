e36c1d1b25db2f006357b9b01abf1773ba9f5a8c
Oksana Molchanova <mksenia728@gmail.com>
2025-01-24 19:30:56 +0100

    end hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-export const getPhoto = (query) => {
+export const getPhoto = (query, page, perPage) => {
   axios.defaults.baseURL = 'https://pixabay.com/api/';
   
   return axios.get('', {params : {    
     key : '48288328-1d3de9be04bab144528bc8ac3',
     q : query,
+    per_page : perPage,
+    page : page,
     image_type : 'photo',
     orientation : 'horizontal',
     safesearch : true,}});
-
 };
--- src/main.js
+++ src/main.js
@@ -7,15 +7,35 @@ import { createLoader } from './js/render-functions';
 
 const formEL = document.querySelector('.form');
 const galleryList = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more-btn');
 const iziToastCommonOptions = { position: 'topRight', messageSize: 20 };
 let simplelightbox;
+let perPage = 15;
+let pageNumber;
+let searchedThema = '';
+const loaderEl = createLoader();
+let galleryItemEl;
+
+const onSimpleLightBox = () => {
+  simplelightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  simplelightbox.refresh();
+};
+
+const onScroll = () => {
+  const { height } = galleryItemEl.getBoundingClientRect();
+  window.scrollBy(0, height * 2);
+};
 
 const createGallery = async e => {
   e.preventDefault();
-  const loaderEl = createLoader();
-  formEL.insertAdjacentElement('afterend', loaderEl);
+  btnLoadMore.classList.add('is-hidden');
+  pageNumber = 1;
+  galleryList.insertAdjacentElement('afterend', loaderEl);
 
-  const searchedThema = e.target.elements[0].value.trim();
+  searchedThema = e.target.elements[0].value.trim();
 
   if (searchedThema === '') {
     iziToast.warning({
@@ -25,14 +45,14 @@ const createGallery = async e => {
     loaderEl.remove();
     return;
   }
-  
+
   try {
     const {
-      data: { hits: photoArray },
-    } = await getPhoto(searchedThema);
+      data: { hits: photoArray, totalHits: maxPhotoCount },
+    } = await getPhoto(searchedThema, pageNumber, perPage);
 
     formEL.reset();
-    loaderEl.remove();
+
     if (photoArray.length === 0) {
       iziToast.show({
         ...iziToastCommonOptions,
@@ -41,22 +61,66 @@ const createGallery = async e => {
       galleryList.innerHTML = '';
       return;
     }
+
     const galleryItems = photoArray
       .map(el => createGalleryElement(el))
       .join('');
     galleryList.innerHTML = galleryItems;
 
-    simplelightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
+    if (pageNumber * perPage < maxPhotoCount) {
+      btnLoadMore.classList.remove('is-hidden');
+      btnLoadMore.addEventListener('click', onLoadMore);
+    }
+
+    onSimpleLightBox();
+  } catch (err) {
+    console.log(err);
+    iziToast.error({
+      ...iziToastCommonOptions,
+      message: err.message,
     });
-    simplelightbox.refresh();
-    
+  } finally {
+    loaderEl.remove();
+  }
+};
+
+const onLoadMore = async () => {
+  try {
+    pageNumber++;
+    btnLoadMore.classList.add('is-hidden');
+    galleryList.insertAdjacentElement('afterend', loaderEl);
+
+    const {
+      data: { hits: photoArray, total: maxPhotoCount },
+    } = await getPhoto(searchedThema, pageNumber, perPage);
+
+    const galleryItems = photoArray
+      .map(el => createGalleryElement(el))
+      .join('');
+    galleryList.insertAdjacentHTML('beforeend', galleryItems);
+
+    galleryItemEl = galleryList.firstChild;
+    onScroll();
+    onSimpleLightBox();
+
+    if (pageNumber * perPage >= maxPhotoCount) {
+      btnLoadMore.classList.add('is-hidden');
+      btnLoadMore.removeEventListener('click', onLoadMore);
+
+      iziToast.show({
+        ...iziToastCommonOptions,
+        message: `Sorry, there are no images matching your search query ${searchedThema}. Please try again!`,
+      });
+    } else {
+      btnLoadMore.classList.remove('is-hidden');
+    }
   } catch (err) {
     iziToast.error({
       ...iziToastCommonOptions,
-      message: err,
+      message: err.message,
     });
+  } finally {
+    loaderEl.remove();
   }
 };
 
